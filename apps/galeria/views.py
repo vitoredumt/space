6747949ddd6b004f148cfc1873efa5772a1a057b
Vitor Eduardo <vitoredumt@gmail.com>
2024-07-15 22:48:57 -0300

    Implementando funcionalidade de editar e deletar imagem

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -48,8 +48,21 @@ def nova_imagem(request):
         
     return render(request, 'galeria/nova_imagem.html', {'form': form})
         
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
 
-def deletar_imagem(request):
-    pass
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Imagem atualizada com sucesso')
+            return redirect('index')
+
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Imagem deletada com sucesso')
+    return redirect('index')
